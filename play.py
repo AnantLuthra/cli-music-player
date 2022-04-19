4cdb712df215fe3b551bb4de7630ea2f93ae96ef
Anant <anant312004@gmail.com>
2022-04-19 19:55:53 +0530

    Initial commit

---
+++ play.py
@@ -0,0 +1,24 @@
+"""
+Author - Anant Luthra
+Date - 19/4/22
+Purpose - To make a CLI music player.
+"""
+
+import os
+import argparse, sys
+
+def get_arg():
+    """This function gets the argument from cli"""
+
+def handle_arg(args):
+    """This function will handle case according to the argument passed by user"""
+
+def play_music(type):
+    """This function will play music as per type, from default directory or current one"""
+
+def stop_music():
+    """This function will stop music"""
+
+if __name__ == "__main__":
+    ...
+    
