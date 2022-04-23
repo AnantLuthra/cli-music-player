2303692bddc8179d7a432631c5cd1ec8edae90b6
Anant <anant312004@gmail.com>
2022-04-23 16:31:18 +0530

    Fixed bugs

--- play.py
+++ play.py
@@ -4,12 +4,10 @@ Date - 19/4/22
 Purpose - To make a CLI music player.
 """
 
-import multiprocessing
-import os
-import argparse, sys
+import multiprocessing, os, argparse, sys, rich
 from playsound import playsound
 
-def read_help():
+def read_help() -> str:
     path = os.getcwd()
     os.chdir(r"E:\Python\Python projects\CLI Music player")
     with open("help.txt", 'r') as f:
@@ -25,11 +23,24 @@ def handle_arg(args):
     elif args.d and args.t:
         play_music("d")
 
+    elif args.c:
+
+        for i in os.listdir(os.getcwd()):
+            if i.endswith(".mp3"): break           
+        else:
+            print("No available music files in the repository.")
+            return
+        
+        
+
+    elif not args.c and not args.d and not args.s:
+        print(read_help())
+
 def play_music(type):
     """This function will play music as per type, from default directory or current one"""
 
     if type == "d":
-        
+
         os.chdir(r"C:\Users\anant luthra")
         p = multiprocessing.Process(target=playsound, args=(r"Downloads\Adam Oh - Trapped In My Mind (Lyrics _ Lyric Video).mp3",))
         p.start()
@@ -43,25 +54,20 @@ def stop_music():
 def get_arg():
     """This function gets the argument from cli"""
 
-    os.chdir(r"E:\Python\Python projects\CLI Music player")
-    with open("help.txt", 'r') as f:
-        HELP = f.read()
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--h", type=bool)
-    parser.add_argument("--d", type=bool, help=HELP, default = False)
-    parser.add_argument("--s", type=str, help=HELP)
-    parser.add_argument("--t", type=bool, help=HELP, default=True)
-    parser.add_argument("--g", type=bool, default=False, help=HELP)
+    parser = argparse.ArgumentParser(description="A Tool that play music from you system", exit_on_error=False)
+    parser.add_argument("--h", action="store_true")
+    parser.add_argument("--d", action="store_true", help="This argument is for playing songs for default directory.", default = False)
+    parser.add_argument("--s", type=str, help="This argument is for playing a specific song through passing it name.")
+    parser.add_argument("--t", action="store_true", help="Set this true for playing song in terminal", default=True)
+    parser.add_argument("--g", action="store_true", default=False, help="Set this true for playing song through windows gui")
+    parser.add_argument("--c", action="store_true", default=False, help="For playing songs from the current directory.")
 
     args = parser.parse_args()
     handle_arg(args)
+
 
 if __name__ == "__main__":
     try:
         get_arg()
     except Exception as e:
         print(e)
-
-    print(os.getcwd())
-
