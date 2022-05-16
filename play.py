b264f88e059bff9edcfe31e03927e24b98260873
Anant <anant312004@gmail.com>
2022-05-16 11:33:41 +0530

    added more things

--- play.py
+++ play.py
@@ -4,7 +4,7 @@ Date - 19/4/22
 Purpose - To make a CLI music player.
 """
 
-import multiprocessing, os, argparse, sys
+import multiprocessing, os, argparse, sys, random
 from rich import print
 from rich.console import Console
 from playsound import playsound
@@ -27,30 +27,33 @@ class player:
         """This function will handle case according to the argument passed by user"""
 
         if args.h == True:
-            self.console.print(self.read_help(), style="blue")
+            self.console.print(self.read_help(), style="bold green")
 
         elif args.d and args.t:
             self.play_music("d")
 
         elif args.c:
 
-            for i in os.listdir(os.getcwd()):
-                if i.endswith(".mp3"): break           
-            else:
-                self.console.print("No available music files in the repository !!", style="bold red")
+            songs = [i for i in os.listdir(os.getcwd()) if i.endswith(".mp3")]
+            
+            if songs == []:
+                style = "bold red on black"
+                self.console.print("\n--------------------Error---------------------",style = style, justify="center")
+                self.console.print("\nNo available music files in the repository !!", style=style, justify="center")
                 return
             
-        
+            os.startfile(random.choice(songs))
+            
         elif not args.c and not args.d and not args.s:
-            self.console.print(self.read_help(), style=" italic blue")
+            self.console.print(self.read_help(), style="green")
 
     def play_music(self, type):
         """This function will play music as per type, from default directory or current one"""
 
         if type == "d":
-
+            print("yaha tak to mai aaya")
             os.chdir(r"C:\Users\anant luthra")
-            p = multiprocessing.Process(target=playsound, args=(r"Downloads\Adam Oh - Trapped In My Mind (Lyrics _ Lyric Video).mp3",))
+            p = multiprocessing.Process(target=playsound, args=("Downloads\\Adam Oh - Trapped In My Mind (Lyrics _ Lyric Video).mp3",))
             p.start()
             # playsound.playsound(r"Downloads\Adam Oh - Trapped In My Mind (Lyrics _ Lyric Video).mp3")
             input("")
@@ -62,7 +65,7 @@ class player:
     def get_arg(self):
         """This function gets the argument from cli"""
 
-        parser = argparse.ArgumentParser(description="A Tool that play music from you system", exit_on_error=True)
+        parser = argparse.ArgumentParser(description="A Tool that play music from you system", exit_on_error=False)
         parser.add_argument("--h", action="store_true")
         parser.add_argument("--d", action="store_true", help="This argument is for playing songs for default directory.", default = False)
         parser.add_argument("--s", type=str, help="This argument is for playing a specific song through passing it name.")
@@ -78,8 +81,8 @@ if __name__ == "__main__":
 
     cli_player = player()
 
-    # try:
-    #     cli_player.get_arg()
-    # except Exception as e:
-    #     print(e)
-    cli_player.get_arg()
+    try:
+        cli_player.get_arg()
+    except Exception as e:
+        print(e)
+    # cli_player.get_arg()
